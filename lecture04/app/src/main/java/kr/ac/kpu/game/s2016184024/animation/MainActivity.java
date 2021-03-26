db1a5cc087cbe5171b9cc31034d6947cbbaf54ca
bongseopshin <llkey12@naver.com>
2021-03-26 12:23:08 +0900

    lectrue 04 - customView

---
+++ lecture04/app/src/main/java/kr/ac/kpu/game/s2016184024/animation/MainActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.kpu.game.s2016184024.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//         MyView mv = new MyView(this);
+//         setContentView(mv);
+    }
+
+}
